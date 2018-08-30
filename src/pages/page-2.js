9bfb021cf072032130810a1b70051bac78723f36
kryptokinght <minanshu.1998@gmail.com>
2018-08-30 16:18:26 +0530

    add more projects

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,13 +6,21 @@ const SecondPage = () => (
     <h2>Some of my Projects Projects</h2>
     <ul>
       <li>
-        <a href="https://github.com/kryptokinght/NipponTour"><h3>Nippon Tour</h3></a>
+        <a href="https://github.com/kryptokinght/NipponTour"><h4>Nippon Tour</h4></a>
         <p>A site to view tour places in Japan, add more, remove some, write reviews for a place, and more... 
           Built using ExpressJs</p>
       </li>
       <li>
-        <a href="https://github.com/kryptokinght/InstagramCatch"><h3>Instagram Catch</h3></a>
+        <a href="https://github.com/kryptokinght/InstagramCatch"><h4>Instagram Catch</h4></a>
         <p>A Chrome Extension that helps you in downloading Instagram pics and videos in one click only.</p>
+      </li>
+      <li>
+        <a href="https://github.com/kryptokinght/weather-react-app"><h4>React-Weather-App</h4></a>
+        <p>Shows live weather data of the major Indian cities which you enter</p>
+      </li>
+      <li>
+        <a href="https://github.com/kryptokinght/react-youtube-clone"><h4>React-Youtube-clone</h4></a>
+        <p>Makes use of the Youtube API to load search results instantly as you type in.</p>
       </li>
     </ul>
     <Link to="/">Go back to the homepage</Link>
