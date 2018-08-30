8c2195bd52edb32c281a1d1bf31eefa9d55a5962
kryptokinght <minanshu.1998@gmail.com>
2018-08-30 13:03:51 +0530

    added my introduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,13 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>My name is Minanshu SIngh aka kryptokinght. I am a full stack web developer 
+      mainly focusing on React, Express and Mongodb. I have contributed in the 
+      webpack.js.org website and looking forward to contribute in more open source open
+      projects.
+    </p>
+    <p></p>
+    <Link to="/page-2/">Go to Hello World</Link>
   </div>
 )
 
